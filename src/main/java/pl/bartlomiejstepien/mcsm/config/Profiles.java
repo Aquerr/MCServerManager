f140733eef8eb704745426f8fca08b82920cd40e
Aquerr <nerdianin@gmail.com>
2022-05-15 16:18:27 +0200

    Added simple postgresql configuration

---
+++ src/main/java/pl/bartlomiejstepien/mcsm/config/Profiles.java
@@ -0,0 +1,8 @@
+package pl.bartlomiejstepien.mcsm.config;
+
+public class Profiles
+{
+    public static final String LOCAL = "local";
+    public static final String H2 = "h2";
+    public static final String POSTGRESQL = "postgresql";
+}
